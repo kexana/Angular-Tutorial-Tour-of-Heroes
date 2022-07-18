37526478fefacd5bbc9e3200383174878e64cc6e
kexana <deotsts@gmail.com>
2022-07-18 11:11:29 +0300

    inital commit

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 12, name: 'Rogue' },
+  { id: 13, name: 'Gambit' },
+  { id: 14, name: 'Spot' },
+  { id: 15, name: 'Rorshaque' },
+  { id: 16, name: 'Sentinel' },
+  { id: 17, name: 'Galactus' },
+  { id: 18, name: 'Spiral' },
+  { id: 19, name: 'Spawn' },
+  { id: 20, name: 'Quicksilver' },
+  { id: 21, name: 'Psylocke' },
+]
